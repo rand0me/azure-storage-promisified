89fc223ee3efc1ed384e45c7ea0376113040e325
rand0me <not.randome@gmail.com>
2017-10-04 12:45:54 +0300

    feat(Project): Initial commit

---
+++ src/TableService.ts
@@ -0,0 +1,36 @@
+import { TableService, common, services } from 'azure-storage';
+
+export class AzureTable {
+    constructor(protected service: TableService, protected table: string) { }
+
+    public createTableIfNotExist(options?: common.RequestOptions) {
+        return new Promise((resolve, reject) =>
+            this.service.createTableIfNotExists(this.table, options, (err, result) =>
+                err ? reject(err) : resolve(result)));
+    }
+
+    public insertEntity(entity: any) {
+        return new Promise((resolve, reject) =>
+            this.service.insertEntity(this.table, entity, (err, result) =>
+                err ? reject(err) : resolve(result)));
+    }
+
+    public insertOrReplaceEntity(entity: any) {
+        return new Promise((resolve, reject) =>
+            this.service.insertOrReplaceEntity(this.table, entity, (err, result) =>
+                err ? reject(err) : resolve(result)));
+    }
+
+    public retrieveEntity<T>(partKey: string, rowKey: string) {
+        return new Promise((resolve, reject) =>
+            this.service.retrieveEntity(this.table, partKey, rowKey, (err, result) =>
+                err ? reject(err) : resolve(result)));
+    }
+
+    public queryEntities<T>(query: services.table.TableQuery, token: TableService.TableContinuationToken) {
+        return new Promise((resolve, reject) =>
+            this.service.queryEntities(this.table, query, token, (err, result) =>
+                err ? reject(err) : resolve(result)));
+    }
+
+}
