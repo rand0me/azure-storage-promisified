9bf7b8f3e2deb7cd26b03df24ac57e58789d9551
rand0me <not.randome@gmail.com>
2017-11-10 13:58:21 +0300

    fix(QueueService): Add missed createMessage method and options params

--- src/QueueService.ts
+++ src/QueueService.ts
@@ -3,9 +3,15 @@ import { QueueService, common, services } from 'azure-storage';
 export class AzureQueue {
     constructor(protected service: QueueService, protected queue: string) { }
 
-    public getMessage() {
+    public createMessage(messageText: any, options?: QueueService.CreateMessageRequestOptions) {
         return new Promise((resolve, reject) =>
-            this.service.getMessage(this.queue, (err, result) => 
+            this.service.createMessage(this.queue, messageText, options, (err, result) =>
+                err ? reject(err) : resolve(result)))
+    }
+
+    public getMessage(options?: QueueService.GetMessageRequestOptions) {
+        return new Promise((resolve, reject) =>
+            this.service.getMessage(this.queue, options, (err, result) => 
                 err ? reject(err) : resolve(result)));
     }
 
